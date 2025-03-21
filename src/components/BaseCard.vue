f34edf01edbba542b0822b32db0264ee1fd4516f
Edgar <edgaarr2005@hotmail.com>
2025-03-21 19:16:42 -0400

    I named slots.

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -1,13 +1,11 @@
 <template>
     <div>
+        <header>
+            <slot name="header"></slot>
+        </header>
         <slot></slot>
     </div>
 </template>
-
-<script>
-export default {
-}
-</script>
 
 <style scoped>
 div {
