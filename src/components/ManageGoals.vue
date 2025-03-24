0cc7871f0f3427db75ea6792f74c135e076e027b
Edgar <edgaarr2005@hotmail.com>
2025-03-23 20:50:12 -0400

    Learned about Fragments. I can have as much top level elements.

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,16 +1,14 @@
 <template>
-    <div>
-        <h2>Manage Goals</h2>
-        <input type="text" ref="goal" />
-        <button @click="setGoal">Set Goal</button>
-        <teleport to="body">
-            <error-alert v-if="inputIsInvalid">
-                <h2>Input is invalid!</h2>
-                <p>Please enter a valid goal.</p>
-                <button @click="inputIsInvalid = false">Okay</button>
-            </error-alert>
-        </teleport>
-    </div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+        <error-alert v-if="inputIsInvalid">
+            <h2>Input is invalid!</h2>
+            <p>Please enter a valid goal.</p>
+            <button @click="inputIsInvalid = false">Okay</button>
+        </error-alert>
+    </teleport>
 </template>
 
 <script>
